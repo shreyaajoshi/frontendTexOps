5f5c1e15c4596ca8d7bffe43eb4d05f0fe9571bf
bipzipgithub <bipind@gmail.com>
2021-03-18 19:25:13 +0000

    port change

--- server.js
+++ server.js
@@ -303,7 +303,7 @@ app.use((err, req, res, next) => {
 });
 
 // specify the port and start listening
-const PORT = process.env.PORT ? process.env.PORT : 8080;
+const PORT = process.env.PORT ? process.env.PORT : 8081;
 const server = app.listen(PORT, () => {
     const host = server.address().address;
     const port = server.address().port;
